ae7bb0c2691a17efbe7ac5a6d5a3b6dc5441a33d
patrickchow0803 <patrickchow0803@gmail.com>
2021-07-13 16:38:00 -0400

    tested add functionalty of the Favorites class

---
+++ test/models/favorites.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test_practice2/models/favorites.dart';
+
+void main() {
+  group('Testing App Provider', () {
+    var favorites = Favorites();
+
+    test('A new item should be added', () {
+      var number = 35;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+    });
+  });
+}
